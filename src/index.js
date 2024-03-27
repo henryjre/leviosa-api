adc351eee6272e6b085aa652784db09ad0d71488
henryjre <pinedahenryjre@gmail.com>
2024-03-28 00:48:59 +0800

    changed port to 8080

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import helmet from "helmet";
 import "dotenv/config";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 import routes from "./routes/index-routes.js";
 
