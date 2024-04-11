03ff6086259d0f1b92c7e1524e7417e429551a5d
henryjre <pinedahenryjre@gmail.com>
2024-04-11 10:18:48 +0800

    changed the code for sql pooling

--- src/sqlPools.js
+++ src/sqlPools.js
@@ -4,11 +4,13 @@ const environment = process.env.node_env;
 
 let caCertificate;
 if (environment === "prod") {
+  console.log("SQL Running on production environment.");
   const fs = require("fs");
   const path = require("path");
   const caCertificatePath = path.join(__dirname, "../cert/DO_Certificate.crt");
   caCertificate = fs.readFileSync(caCertificatePath);
 } else if (environment === "dev") {
+  console.log("SQL Running on development environment.");
   caCertificate = process.env.db_cert;
 }
 
@@ -17,26 +19,34 @@ const dbPort = process.env.db_port;
 const dbUsername = process.env.db_username;
 const dbPassword = process.env.db_password;
 
-function createPool(database) {
-  return mysql.createPool({
-    host: dbHost,
-    port: dbPort,
-    user: dbUsername,
-    password: dbPassword,
-    database,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    ssl: {
-      ca: caCertificate,
-      rejectUnauthorized: true,
-    },
-  });
-}
+const commonPoolConfig = {
+  host: dbHost,
+  port: dbPort,
+  user: dbUsername,
+  password: dbPassword,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  ssl: {
+    ca: caCertificate,
+    rejectUnauthorized: true,
+  },
+};
 
-const managementPool = createPool("management");
-const leviosaPool = createPool("defaultdb");
-const inventoryPool = createPool("inventory");
+const managementPool = mysql.createPool({
+  ...commonPoolConfig,
+  database: "management",
+});
+
+const leviosaPool = mysql.createPool({
+  ...commonPoolConfig,
+  database: "defaultdb",
+});
+
+const inventoryPool = mysql.createPool({
+  ...commonPoolConfig,
+  database: "inventory",
+});
 
 const pools = { managementPool, leviosaPool, inventoryPool };
 
