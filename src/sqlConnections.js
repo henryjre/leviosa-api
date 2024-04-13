6d7498196a92f73b8cf654db8231faa9035bc2e7
henryjre <pinedahenryjre@gmail.com>
2024-04-13 18:25:27 +0800

    changed the db_cert to an env variable

--- src/sqlConnections.js
+++ src/sqlConnections.js
@@ -1,23 +1,10 @@
 import mysql from "mysql2/promise";
 
-const environment = process.env.node_env;
-
-let caCertificate;
-if (environment === "prod") {
-  console.log("SQL Running on production environment.");
-  const fs = require("fs");
-  const path = require("path");
-  const caCertificatePath = path.join(__dirname, "../cert/DO_Certificate.crt");
-  caCertificate = fs.readFileSync(caCertificatePath);
-} else if (environment === "dev") {
-  console.log("SQL Running on development environment.");
-  caCertificate = process.env.db_cert;
-}
-
 const dbHost = process.env.db_host;
 const dbPort = process.env.db_port;
 const dbUsername = process.env.db_username;
 const dbPassword = process.env.db_password;
+const caCertificate = process.env.db_cert;
 
 const commonPoolConfig = {
   host: dbHost,
