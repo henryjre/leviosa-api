68a0f3adb15915b58b427c7bef85d324df5671fc
henryjre <pinedahenryjre@gmail.com>
2024-10-25 00:01:40 +0800

    [UPDATE] added cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // Bring in our dependencies
 import express from "express";
 import helmet from "helmet";
+import cors from "cors";
+
 import "dotenv/config";
 import "./functions/cron.js";
 
@@ -11,6 +13,7 @@ import routes from "./routes/index-routes.js";
 
 app.use(helmet());
 app.use(express.json());
+app.use(cors());
 
 // "/api"
 app.use("/api", routes);
