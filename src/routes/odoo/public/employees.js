14603aa23788834b4d6808e4e0a651797b8e6de4
henryjre <pinedahenryjre@gmail.com>
2024-11-25 23:01:38 +0800

    [UPDATE] added endpoint for getting and calculating the daily sales quota

--- src/routes/odoo/public/employees.js
+++ src/routes/odoo/public/employees.js
@@ -71,7 +71,134 @@ export async function getAverageTransactionValue(req, res) {
   }
 }
 
-function getStartAndEndOfDay(date) {
+export async function getOrderSalesJournal(req, res) {
+  const { date } = req.body;
+
+  if (!date) {
+    return res
+      .status(400)
+      .json({ ok: false, message: "No date specified", data: [] });
+  }
+
+  const { start_date, end_date } = getStartAndEndOfDay(date);
+
+  try {
+    const sessions = await getSessions(start_date, end_date);
+
+    if (!sessions.ok) {
+      throw new Error(sessions.message);
+    }
+
+    const sessionData = sessions.data;
+    const sessionNames = sessionData.map((item) => item.display_name);
+
+    const sessionJournals = await getSessionJournalEntries(sessionNames);
+
+    if (!sessionJournals.ok) {
+      throw new Error(sessionJournals.message);
+    }
+
+    const fullSessionCombined = combineArrays(
+      sessionData,
+      sessionJournals.data
+    );
+    const fullTotalSession = calculateTotal(fullSessionCombined);
+
+    return res
+      .status(200)
+      .json({ ok: true, message: "success", data: fullTotalSession });
+  } catch (error) {
+    console.error("Error:", error.message);
+    return res
+      .status(404)
+      .json({ ok: false, message: error.message, data: [] });
+  }
+
+  async function getSessions(dateStart, dateEnd) {
+    try {
+      const params = {
+        model: "pos.session",
+        method: "search_read",
+        domain: [
+          ["start_at", ">=", dateStart],
+          ["stop_at", "<=", dateEnd],
+          ["state", "=", "closed"],
+        ],
+        fields: ["company_id", "display_name"],
+      };
+
+      const request = await jsonRpc("call", {
+        service: "object",
+        method: "execute",
+        args: [
+          dbName,
+          2,
+          password,
+          params.model,
+          params.method,
+          params.domain,
+          params.fields,
+        ],
+      });
+
+      if (request.error) {
+        throw new Error("rpc_error");
+      }
+
+      if (!request.result.length) {
+        throw new Error("no_data_found");
+      }
+
+      return { ok: true, message: "success", data: request.result };
+    } catch (error) {
+      console.error("Error:", error.message);
+      return { ok: false, message: error.message, data: [] };
+    }
+  }
+
+  async function getSessionJournalEntries(sessionNames) {
+    try {
+      const params = {
+        model: "account.move.line",
+        method: "search_read",
+        domain: [["ref", "in", sessionNames]],
+        fields: ["move_name", "account_id", "ref", "name", "debit", "credit"],
+      };
+
+      const request = await jsonRpc("call", {
+        service: "object",
+        method: "execute",
+        args: [
+          dbName,
+          2,
+          password,
+          params.model,
+          params.method,
+          params.domain,
+          params.fields,
+        ],
+      });
+
+      if (request.error) {
+        throw new Error("rpc_error");
+      }
+
+      if (!request.result.length) {
+        throw new Error("no_data_found");
+      }
+
+      return { ok: true, message: "success", data: request.result };
+    } catch (error) {
+      console.error("Error:", error.message);
+      return { ok: false, message: error.message, data: [] };
+    }
+  }
+}
+
+// EXTERIOR FUNCTIONS
+
+function getStartAndEndOfDay(dateString) {
+  const date = new Date(dateString);
   // Use moment to parse the input date and set the time zone to Philippine Time (Asia/Manila)
   let startOfDay = moment(date).tz("Asia/Manila").startOf("day"); // Set to 12:00:00 AM
   let endOfDay = moment(date).tz("Asia/Manila").endOf("day"); // Set to 11:59:59 PM
@@ -113,3 +240,41 @@ function getTotalATVAmount(data) {
     }))
     .sort((a, b) => b.average_amount - a.average_amount);
 }
+
+// FOR DAILY SALES QUOTA
+const combineArrays = (array1, array2) => {
+  return array1.map((item) => {
+    const journalEntries = array2.filter(
+      (entry) => entry.ref === item.display_name
+    );
+    return {
+      ...item,
+      journal_entries: journalEntries,
+    };
+  });
+};
+
+const calculateTotal = (data) => {
+  return data.map((session) => {
+    let totalSales = 0;
+    let totalRefunds = 0;
+
+    session.journal_entries.forEach(({ name, debit, credit }) => {
+      if (name && name.trim().toLowerCase() === "sales untaxed") {
+        totalSales += credit;
+      } else if (name && name.trim().toLowerCase() === "refund untaxed") {
+        totalRefunds += debit;
+      }
+    });
+
+    const totalDifference = totalSales - totalRefunds;
+
+    return {
+      company_id: session.company_id,
+      display_name: session.display_name,
+      total_sales: totalSales,
+      total_deductibles: totalRefunds,
+      total_difference: totalDifference,
+    };
+  });
+};
--- src/routes/odoo/public/index-public.js
+++ src/routes/odoo/public/index-public.js
@@ -3,7 +3,10 @@ const publicEndpoint = Router();
 
 import { getLoyaltyCardData } from "./loyalty_card.js";
 import { getLoyaltyRewards } from "./discount.js";
-import { getAverageTransactionValue } from "./employees.js";
+import {
+  getAverageTransactionValue,
+  getOrderSalesJournal,
+} from "./employees.js";
 
 // "/api/odoo/public/getLoyaltyCardData"
 publicEndpoint.get("/getLoyaltyCardData", getLoyaltyCardData);
@@ -13,5 +16,8 @@ publicEndpoint.get("/getLoyaltyRewards", getLoyaltyRewards);
 
 // "/api/odoo/public/getATV"
 publicEndpoint.post("/getATV", getAverageTransactionValue);
+
+// "/api/odoo/public/getOrderSales"
+publicEndpoint.post("/getOrderSales", getOrderSalesJournal);
 
 export default publicEndpoint;
